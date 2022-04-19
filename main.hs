83a2bd47c4aace7969f8cbfd1c145e978b8ab515
Gísli Kristjánsson <gislik@hamstur.is>
2022-04-19 22:11:15 +0000

    feat: print consolidated orderbook

--- main.hs
+++ main.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Exchange where 
 
-import Data.Foldable (mapM_)
+import Data.Monoid (Sum(..))
+import Data.Foldable (mapM_, foldMap)
 import Data.Typeable (Typeable, typeOf)
-import Data.List (insertBy)
+import Data.List (insertBy, groupBy, nubBy)
+import qualified Data.List.NonEmpty as NonEmpty
+import qualified Data.List.NonEmpty as List (NonEmpty)
 import Data.Function (on)
 import Data.Ord (Down(Down), comparing)
 
@@ -21,32 +26,44 @@ newtype Amount =
   Amount Double 
     deriving (Show, Eq)
 
+instance Semigroup Amount where
+  Amount d <> Amount e = Amount (d + e)
+  
+instance Monoid Amount where
+  mempty = Amount 0.0
+
 -- Price
 newtype Price = 
   Price Double 
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Num)
 
 -- Time
 newtype Time = 
   Time Int
+    deriving (Show, Eq)
 
 -- Order
 data Order asset = 
-    Bid asset Amount Price
-  | Ask asset Amount Price
+    Bid asset Time Amount Price
+  | Ask asset Time Amount Price
   deriving (Show, Eq)
 
+
 amountOf :: Order asset -> Amount
-amountOf (Bid _ amount _) = amount
-amountOf (Ask _ amount _) = amount
+amountOf (Bid _ _ amount _) = amount
+amountOf (Ask _ _ amount _) = amount
 
 priceOf :: Order asset -> Price
-priceOf (Bid _ _ price) = price
-priceOf (Ask _ _ price) = price
+priceOf (Bid _ _ _ price) = price
+priceOf (Ask _ _ _ price) = price
+
+timeOf :: Order asset -> Time
+timeOf (Bid _ time _ _) = time
+timeOf (Ask _ time _ _) = time
 
 typeOfOrder :: Order asset -> String
-typeOfOrder (Bid _ _ _) = "Bid"
-typeOfOrder (Ask _ _ _) = "Ask"
+typeOfOrder (Bid _ _ _ _) = "Bid"
+typeOfOrder (Ask _ _ _ _) = "Ask"
 
 
 printOrder :: Typeable asset => Order asset -> IO ()
@@ -55,6 +72,23 @@ printOrder order = do
   putStr $ " (" ++ show (priceOf order) ++ ")"
   putStr $ " (" ++ show (amountOf order) ++ ")"
   putStrLn ""
+
+sumOrderAmount :: List.NonEmpty (Order asset) -> Order asset
+sumOrderAmount orders = 
+  let
+    order = NonEmpty.head orders
+    amount = foldMap amountOf orders
+  in
+    case order of
+      Bid asset time _  price -> Bid asset time amount price
+      Ask asset time _  price -> Ask asset time amount price
+
+groupOrdersBy :: Eq b => (Order asset -> b) -> [Order asset] -> [List.NonEmpty (Order asset)]
+groupOrdersBy f orders = 
+  NonEmpty.fromList <$> groupBy (equalOn f) orders
+
+equalOn :: Eq b => (a -> b) -> a -> a -> Bool
+equalOn f = (==) `on` f
 
 -- Book
 data Book asset = Book {
@@ -67,36 +101,32 @@ printBook :: (Show asset, Typeable asset) => Book asset -> IO ()
 printBook book = do
   let typeOfBook = show (typeOf book)
   putStrLn typeOfBook
-  -- putStrLn (take (length typeOfBook) (repeat '='))
   putStrLn (replicate (length typeOfBook) '=')
-  mapM_ printOrder (asks book)
+  mapM_ printOrder $ 
+    sumOrderAmount <$> groupOrdersBy priceOf (asks book)
   putStrLn ""
-  mapM_ printOrder (reverse (bids book))
+  mapM_ printOrder $ 
+    sumOrderAmount <$> groupOrdersBy priceOf (reverse (bids book))
   putStrLn ""
-
--- instance Semigroup (Book asset) where
---   (Book bs1 as1) <> (Book bs2 as2) = Book (bs1 <> bs2) (as1 <> as2)
--- 
--- instance Monoid (Book asset) where
---   mempty = emptyBook
---   mappend = (<>)
 
 emptyBook :: Book asset
 emptyBook = Book [] []
 
 newOrder :: Order asset -> Book asset -> Book asset
-newOrder order@(Bid _ _ _) book = 
+newOrder order@(Bid _ _ _ _) book = 
   book { bids = insertBy (comparing priceOf) order (bids book) }
-newOrder order@(Ask _ _ _) book = 
+newOrder order@(Ask _ _ _ _) book = 
   book { asks = insertBy (comparing (Down . priceOf)) order (asks book) }
 
-bid1 = Bid BTC (Amount 1) (Price 1000)
-bid2 = Bid BTC (Amount 0.5) (Price 1100)
-bid3 = Bid BTC (Amount 0.6) (Price 900)
-ask1 = Ask BTC (Amount 0.5) (Price 1500)
-ask2 = Ask BTC (Amount 0.2) (Price 1500)
-ask3 = Ask BTC (Amount 0.1) (Price 1400)
+
+bid1 = Bid BTC (Time 0) (Amount 1.0) (Price 1000)
+bid2 = Bid BTC (Time 0) (Amount 0.5) (Price 1100)
+bid3 = Bid BTC (Time 0) (Amount 0.6) (Price 900)
+ask1 = Ask BTC (Time 0) (Amount 0.5) (Price 1500)
+ask2 = Ask BTC (Time 0) (Amount 0.2) (Price 1500)
+ask3 = Ask BTC (Time 0) (Amount 0.1) (Price 1400)
 
 book = foldr newOrder emptyBook [bid1, bid2, ask1, ask2, ask3]
+
 
 
