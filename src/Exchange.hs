c4056879757f84c55ae9a3c6003711e6791cacf6
Gísli Kristjánsson <gislik@hamstur.is>
2022-04-21 11:53:06 +0000

    fix: reading orders

--- src/Exchange.hs
+++ src/Exchange.hs
@@ -5,24 +5,46 @@
 
 module Exchange where 
 
+import qualified Data.Char as Char
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.List.NonEmpty as List (NonEmpty)
 import qualified Control.Monad.Trans.State.Strict as State
+import qualified Text.ParserCombinators.ReadP as Read
+import GHC.Read (Read, readPrec)
 import Data.Typeable (Typeable, typeOf)
 import Data.List (insertBy, groupBy)
 import Data.Function (on)
 import Data.Ord (Down(Down), comparing)
+import Text.ParserCombinators.ReadP (ReadP, (+++))
 import Control.Monad.Trans.State.Strict (StateT)
+import Data.Monoid (Ap(..))
 
 -- Assets
 data ETH = 
   ETH   
-    deriving (Show, Eq, Read)
+    deriving (Show, Eq)
+
+instance Read ETH where
+  readsPrec _ = 
+    Read.readP_to_S $
+      readString "ETH" ETH
 
 data BTC = 
   BTC 
-    deriving (Show, Eq, Read)
+    deriving (Show, Eq)
 
+instance Read BTC where
+  readsPrec _ = 
+    Read.readP_to_S $
+      readString "BTC" BTC
+
+readString :: String -> asset -> ReadP asset
+readString s x = do
+    Read.skipSpaces
+    str <- Read.munch1 (Char.isAlphaNum)
+    if map Char.toUpper str == map Char.toUpper s
+      then return x
+      else Read.pfail
 
 -- Amount
 newtype Amount = 
@@ -50,7 +72,12 @@ newtype Time =
 data Side =
     Bid 
   | Ask
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq)
+
+instance Read Side where
+  readsPrec _ = 
+    Read.readP_to_S $
+      readString "Bid" Bid +++ readString "Ask" Ask
 
 -- Entry
 class Entry a asset where
